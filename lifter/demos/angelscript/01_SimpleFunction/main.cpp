549fb21cd478db9559e29dce5ddc3a8347af21d0
Koen Samyn <samyn.koen@gmail.com>
2026-08-26 07:37:35 +0200

    Reflection of a bytecode function
    Added a new demo

--- lifter/demos/angelscript/01_SimpleFunction/main.cpp
+++ lifter/demos/angelscript/01_SimpleFunction/main.cpp
@@ -5,92 +5,29 @@
 #include <cstdint>
 #include <iostream>
 
-struct VM {
-    // Parameters, locals and compiler-generated temporaries all live here.
-    std::array<std::int32_t, 256> variables{};
+#include "asbc/frame.hpp"
+#include "asbc/execute.hpp"
+#include "asbc/block.hpp"
 
-    // Four-byte view of AngelScript's value register.
-    std::uint32_t valueRegister{};
 
-    // RET metadata. It will matter once we implement nested calls.
-    std::uint16_t argumentWordsToPop{};
-
-    bool running{true};
-};
-
-template<
-    asEBCInstr Op,
-    std::int32_t Arg0 = 0,
-    std::int32_t Arg1 = 0,
-    std::int32_t Arg2 = 0
->
-constexpr void execute(VM& vm)
-{
-    if constexpr (Op == asBC_SUSPEND) {
-        // AngelScript uses this for line callbacks and suspension.
-        // The first AOT implementation does not support suspension.
-    }
-    else if constexpr (Op == asBC_MULi) {
-        constexpr auto destination =
-            static_cast<std::size_t>(Arg0);
-
-        constexpr auto left =
-            static_cast<std::size_t>(Arg1);
-
-        constexpr auto right =
-            static_cast<std::size_t>(Arg2);
-
-        vm.variables[destination] =
-            vm.variables[left] * vm.variables[right];
-    }
-    else if constexpr (Op == asBC_CpyVtoR4) {
-        constexpr auto source =
-            static_cast<std::size_t>(Arg0);
-
-        vm.valueRegister =
-            std::bit_cast<std::uint32_t>(
-                vm.variables[source]
-            );
-    }
-    else if constexpr (Op == asBC_RET) {
-        vm.argumentWordsToPop =
-            static_cast<std::uint16_t>(Arg0);
-
-        vm.running = false;
-    }
-    else {
-        static_assert(
-            Op == asBC_SUSPEND ||
-            Op == asBC_MULi ||
-            Op == asBC_CpyVtoR4 ||
-            Op == asBC_RET,
-            "AngelScript opcode has not been implemented"
-        );
-    }
-}
-
-template<auto... Steps>
-constexpr void block(VM& vm)
-{
-    (Steps(vm), ...);
-}
 
 constexpr std::int32_t square(std::int32_t value)
 {
-    VM vm;
+    using namespace asbc;
+    Frame frame;
 
     // AngelScript variable slot 0 contains the parameter.
-    vm.variables[0] = value;
+    frame.variables[0] = value;
 
     block<
         execute<asBC_SUSPEND>,
         execute<asBC_MULi, 1, 0, 0>,
         execute<asBC_CpyVtoR4, 1>,
         execute<asBC_RET, 1>
-    >(vm);
+    >(frame);
 
     return std::bit_cast<std::int32_t>(
-        vm.valueRegister
+        frame.valueRegister
     );
 }
 
---
+++ lifter/demos/angelscript/02_SimpleFunctionReflection/main.cpp
@@ -0,0 +1,92 @@
+#include <meta>
+#include <iostream>
+
+#include "asbc/decode.hpp"
+#include "asbc/transform.hpp"
+
+// int square(int)
+inline constexpr std::array<std::uint32_t, 5> square_bytecode_0{
+    0x0000003Fu,
+    0x00010075u,
+    0x00000000u,
+    0x00010052u,
+    0x0001000Au
+};
+
+constexpr std::int32_t generatedSquare(std::int32_t value)
+{
+    asbc::Frame frame;
+    frame.variables[0] = value;
+
+    constexpr std::meta::info program =
+        asbc::createBlockInfo<square_bytecode_0>();
+
+    [:program:](frame);
+
+    return std::bit_cast<std::int32_t>(
+        frame.valueRegister
+    );
+}
+
+static_assert(generatedSquare(0) == 0);
+static_assert(generatedSquare(7) == 49);
+static_assert(generatedSquare(-8) == 64);
+
+int main()
+{
+    constexpr auto offsets =
+        std::define_static_array(
+            asbc::createInstructionOffsets<
+                square_bytecode_0
+            >()
+        );
+
+    static_assert(offsets.size() == 4);
+    static_assert(offsets[0] == 0);
+    static_assert(offsets[1] == 1);
+    static_assert(offsets[2] == 3);
+    static_assert(offsets[3] == 4);
+
+    constexpr auto suspend =
+        asbc::decodeOperands<square_bytecode_0>(offsets[0]);
+
+    static_assert(suspend.arg0 == 0);
+    static_assert(suspend.arg1 == 0);
+    static_assert(suspend.arg2 == 0);
+
+    uint16_t arg0 = static_cast<uint16_t>(0x00010075u >> 16);
+    std::cout << "arg 0 " << arg0 << std::endl;
+
+    uint16_t arg2 = asbc::unsignedHighWord(0x00010075u);
+    std::cout << "arg 2 " << arg2 << std::endl;
+
+    constexpr auto multiply =
+        asbc::decodeOperands<square_bytecode_0>(offsets[1]);
+
+    static_assert(multiply.arg0 == 1);
+    static_assert(multiply.arg1 == 0);
+    static_assert(multiply.arg2 == 0);
+
+    constexpr auto copyReturn =
+        asbc::decodeOperands<square_bytecode_0>(offsets[2]);
+
+    static_assert(copyReturn.arg0 == 1);
+
+    constexpr auto returnInstruction =
+        asbc::decodeOperands<square_bytecode_0>(offsets[3]);
+
+    static_assert(returnInstruction.arg0 == 1);
+
+    std::cout
+        << "MULi dst=" << multiply.arg0
+        << " src1=" << multiply.arg1
+        << " src2=" << multiply.arg2
+        << '\n';
+
+    std::cout
+        << "generatedSquare(12) = "
+        << generatedSquare(12)
+        << '\n';
+    
+    return 0;
+}
