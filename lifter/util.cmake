d36892da0e7be959c88dee65afddeb64db3b98ae
Koen Samyn <samyn.koen@gmail.com>
2026-08-26 07:36:13 +0200

    Update util.cmake
    Added function to configure a reflection project

--- lifter/util.cmake
+++ lifter/util.cmake
@@ -33,3 +33,73 @@ function(ConfigureASProject ProjectName PROJECT_FOLDER )
            
     )
 endfunction()
+
+function(ConfigureASReflectionProject ProjectName PROJECT_FOLDER)
+    target_link_libraries(
+        ${ProjectName}
+        PUBLIC
+            asbc
+            angelscript
+    )
+
+    add_dependencies(
+        ${ProjectName}
+        as_resources
+    )
+
+    # Require standard C++26 rather than GNU extensions.
+    set_target_properties(
+        ${ProjectName}
+        PROPERTIES
+            CXX_STANDARD          26
+            CXX_STANDARD_REQUIRED YES
+            CXX_EXTENSIONS        NO
+    )
+
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 16.0)
+            message(
+                FATAL_ERROR
+                "${ProjectName} requires GCC 16.0 or newer; "
+                "found GCC ${CMAKE_CXX_COMPILER_VERSION}"
+            )
+        endif()
+
+        # Still required for reflection support in GCC 16.1.
+        target_compile_options(
+            ${ProjectName}
+            PRIVATE
+                $<$<COMPILE_LANGUAGE:CXX>:-freflection>
+        )
+    else()
+        message(
+            FATAL_ERROR
+            "${ProjectName} currently requires GCC 16.1 with -freflection; "
+            "found ${CMAKE_CXX_COMPILER_ID} "
+            "${CMAKE_CXX_COMPILER_VERSION}"
+        )
+    endif()
+
+    message(STATUS "Debugger working dir = ${AS_BIN_DIR}")
+
+    set_target_properties(
+        ${ProjectName}
+        PROPERTIES
+            FOLDER "${PROJECT_FOLDER}"
+
+            RUNTIME_OUTPUT_DIRECTORY_DEBUG
+                "${AS_BIN_DIR}"
+
+            RUNTIME_OUTPUT_DIRECTORY_RELEASE
+                "${AS_BIN_DIR}"
+
+            RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO
+                "${AS_BIN_DIR}"
+
+            RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL
+                "${AS_BIN_DIR}"
+
+            PDB_OUTPUT_DIRECTORY
+                "${AS_BIN_DIR}"
+    )
+endfunction()
